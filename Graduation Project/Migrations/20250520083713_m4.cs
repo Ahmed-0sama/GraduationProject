6484908f3fbcfd7584e7ed90d12f31cc701ccc07
ahmed osama <ahmedosama200266@outlook.com>
2025-05-24 11:57:06 +0300

    edit the connection string

---
+++ Graduation Project/Migrations/20250520083713_m4.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Graduation_Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class m4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Expenses_AspNetUsers_UserId",
+                table: "Expenses");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "Expenses",
+                newName: "userId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Expenses_UserId",
+                table: "Expenses",
+                newName: "IX_Expenses_userId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Expenses_AspNetUsers_userId",
+                table: "Expenses",
+                column: "userId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Expenses_AspNetUsers_userId",
+                table: "Expenses");
+
+            migrationBuilder.RenameColumn(
+                name: "userId",
+                table: "Expenses",
+                newName: "UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Expenses_userId",
+                table: "Expenses",
+                newName: "IX_Expenses_UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Expenses_AspNetUsers_UserId",
+                table: "Expenses",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
